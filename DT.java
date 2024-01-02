afe38908d483b75703cb7cea0f6d61acd06de95c
Amey Murkar <146580786+ameyycodess@users.noreply.github.com>
2024-01-02 23:20:34 +0530

    Add files via upload

---
+++ DT.java
@@ -0,0 +1,25 @@
+class DT
+{
+    public static void main(String[] args) {
+        int a=2;
+        byte var=21;
+        char ch1= 'a';
+        long sy = 36000;
+        short s =12;
+        //float f = 1.2;
+        float f = 1.2f;
+        double d= 2.52;
+        boolean b=true;
+        boolean b1=false;
+
+        System.out.println("Int="+a);
+        System.out.println("byte="+var);
+        System.out.println("char="+ch1);
+        System.out.println("Long="+sy);
+        System.out.println("short="+s);
+        System.out.println("float="+f);
+        System.out.println("double="+d);
+        System.out.println("boolean 1="+b);
+        System.out.println("boolean 2="+b1);
+    }
+}
