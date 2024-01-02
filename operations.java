bdbaa548f7cce40e6428329ef05f983618e1b37c
Amey Murkar <146580786+ameyycodess@users.noreply.github.com>
2024-01-03 00:11:32 +0530

    Add files via upload

---
+++ operations.java
@@ -0,0 +1,62 @@
+class operations 
+{
+    public static void main(String[] args) {
+    int a = 20;
+    int b = 10;
+//arithmetic operations
+    int c = a+b;
+    int d = a-b;
+    int e = a*b;
+    int f = a%b;
+
+    //relational operators
+    boolean g = a>b;
+    boolean h = a<b;
+    boolean i = a>=b;
+    boolean j = a<=b;
+    boolean j2 = a!=b;
+
+    //logical operations
+    boolean k = true;
+    boolean l =false;
+    boolean m = k && l;
+    boolean n = k || l;
+    
+    //shift operators
+    int o = a>>2;
+    int p = b<<2;
+    int q = a>>>2;
+
+    //bitwise operators
+    int r = a & b;
+    int s = a|b;
+    //int t  = a<>1;
+
+
+
+    
+    System.out.println(c);
+    System.out.println(d);
+    System.out.println(e);
+    System.out.println(f);
+
+    System.out.println(g);
+    System.out.println(h);
+    System.out.println(i);
+    System.out.println(j);
+    System.out.println(j2);
+    
+    System.out.println(m);
+    System.out.println(n);
+    
+
+    System.out.println(o);
+    System.out.println(p);
+    System.out.println(q);
+
+    System.out.println(r);
+    System.out.println(s);
+    //System.out.println(t);
+
+    } 
+}
---
+++ val.java
@@ -0,0 +1,26 @@
+class val 
+{
+    public static void main(String[] args) {
+        
+    //System.out.println(int.MAX_VALUE);
+    System.out.println("Max Int:" + Integer.MAX_VALUE);
+    System.out.println("Min Int:" + Integer.MIN_VALUE);
+
+    System.out.println("Max Double:" + Double.MAX_VALUE);
+    System.out.println("Min Double:" + Double.MIN_VALUE);
+
+    System.out.println("Max Short:" + Short.MAX_VALUE);
+    System.out.println("Min Short:" + Short.MAX_VALUE);
+
+    System.out.println("Max byte:" + Byte.MAX_VALUE);
+    System.out.println("Min byte:" + Byte.MAX_VALUE);
+
+    System.out.println("Max Long:" + Long.MAX_VALUE);
+    System.out.println("Min Long:" + Long.MAX_VALUE);
+
+    System.out.println("Max Float:" + Float.MAX_VALUE);
+    System.out.println("Min Float:" + Float.MAX_VALUE);
+
+    }
+    
+}
